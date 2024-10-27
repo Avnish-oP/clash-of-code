82788e1f28e4870a59922e5440d9fedf3856e455
Avnish-oP <kavnish1245@gmail.com>
2024-10-27 18:37:02 +0530

    hero section changes done and navbar fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import AboutClashOfCode from "@/components/AboutUs";
-// import Hero from "@/components/Hero";
 import Hero2 from "@/components/Hero2";
 import Organizers from "@/components/Organizers";
 import Schedule from "@/components/Schedule";
@@ -9,7 +8,6 @@ import Tracks from "@/components/Tracks";
 export default function Home() {
   return (
     <div className="">
-      {/* <Hero /> */}
       <Hero2 />
       <AboutClashOfCode />
       <Tracks />
